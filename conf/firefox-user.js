685b64d5a789557ea237783d41f39239b8bc0400
LittleboyHarry <littleboyharry@qq.com>
2022-05-26 12:48:31 +0800

    .

---
+++ conf/firefox-user.js
@@ -0,0 +1,20 @@
+// Homepage
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.topSitesRows", 2);
+user_pref("browser.newtabpage.activity-stream.section.highlights.rows", 3);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", true);
+
+// Https Only
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
+
+// Behaviour
+user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("browser.aboutConfig.showWarning", false);
+
+// Urlbar
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.placeholderName", "Bing");
+user_pref("browser.urlbar.placeholderName.private", "Bing");
